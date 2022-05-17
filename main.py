e50c80ac68c4d76d7fcb1eb477ce9e60e3fe2188
Kevin Marsh <kevin.marsh@gmail.com>
2022-05-17 10:29:03 -0400

    Adds a random experiment

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from autogeoreferencer import find_candidate_cpps
 
 # Test set of major road intersection points in Champaign County, Ohio
 vector = np.loadtxt("data/39021-intersections-3857.psv", delimiter="|")
-raster = np.loadtxt("data/OH-Champaign-72-01.csv", delimiter=",")
+raster = np.loadtxt("data/OH-Champaign-72-01.csv", delimiter=",", usecols=(0, 1))
 
 # TODO: Create a ground truth mapping of raster point to vector point
 
